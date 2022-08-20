d423138be0bab1c5730000038a630bb7c05f4c63
ponbac <root@ponbac.xyz>
2022-08-20 11:07:39 +0200

    logo sizing

--- src/components/UpcomingGames.tsx
+++ src/components/UpcomingGames.tsx
@@ -73,18 +73,18 @@ const UpcomingGame = (props: UpcomingGameProps) => {
 
   return (
     <Link to={`/game/${game.id}`}>
-      <div className="flex flex-col items-center justify-center font-novaMono space-y-2 hover:bg-gray-700/70 rounded-xl transition-all p-2 max-h-34">
+      <div className="flex flex-col items-center justify-center font-novaMono space-y-2 hover:bg-gray-700/70 rounded-xl transition-all p-2">
         <div className="flex flex-row gap-4 justify-center items-center">
           <TeamFlag
             team={game.homeTeam}
             width={"3.5rem"}
-            className="rounded-md"
+            className="rounded-md h-16"
           />
           <p className="font-bold text-3xl">vs</p>
           <TeamFlag
             team={game.awayTeam}
             width={"3.5rem"}
-            className="rounded-md"
+            className="rounded-md h-16"
           />
         </div>
         <div className="flex flex-col items-center justify-center">
