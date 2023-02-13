5f63f684be333f64ecfb4539dae7738a7b141b37
ponbac <root@ponbac.xyz>
2023-02-13 18:00:21 +0100

    some copy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const APP_URL = (): string =>
-  window.location.host == "localhost:5173"
+  window.location.host === "localhost:5173"
     ? "http://localhost:5173"
     : "https://cl.backman.app";
 
@@ -8,7 +8,7 @@ export const GROUPS = ["A", "B", "C", "D", "E", "F", "G", "H"];
 // TODO: This is a hack, but it works for now (should be server-side).
 export const GROUP_PREDICTIONS_CLOSE = "2022-09-06T17:00:00";
 export const PLAYOFF_PREDICTIONS_OPEN = "2022-11-06T20:00:00";
-export const PLAYOFF_PREDICTIONS_CLOSE = "2023-02-14T20:00:00";
+export const PLAYOFF_PREDICTIONS_CLOSE = "2023-02-14T20:59:00";
 
 export const ADMIN_ID = "eb8fbd9c-13b6-4c32-8812-efa40be059b5";
 
